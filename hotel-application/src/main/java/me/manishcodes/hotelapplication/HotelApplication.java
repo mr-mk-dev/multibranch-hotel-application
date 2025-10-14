35462bb8ac1462d28985c9c0226b0ce71248c13b
Manish <itsmenishan.12@gmail.com>
2025-10-15 00:39:56 +0530

    added hotel application

---
+++ hotel-application/src/main/java/me/manishcodes/hotelapplication/HotelApplication.java
@@ -0,0 +1,13 @@
+package me.manishcodes.hotelapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+	}
+
+}
